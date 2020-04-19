76c31c6e6045252716dda770a3cbdac7a8c1c4d6
YCALLE <yeison.calle@pragma.com.co>
2020-04-18 20:26:39 -0500

    Chulesaurios Select

--- src/Components/Curriculum/Curriculum.js
+++ src/Components/Curriculum/Curriculum.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import {Form, FormGroup, Label, Input, FormText, Button } from 'reactstrap';
 import { consts } from '../../constants/constants';
+import MultipleSelection from '../MultipleSelection/MultipleSelection';
 
 const Curriculum = ({ type }) => {
   const { DEPARTAMENTAL, MUNICIPAL } = consts;
+  const options = [
+    {
+      name: 'Chulesaurio Rex'
+    },
+    {
+      name: 'Chulesirraptor'
+    }
+  ]
   
   return (
     type &&
@@ -16,28 +25,10 @@ const Curriculum = ({ type }) => {
           <Label for="examplePassword">Password</Label>
           <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
         </FormGroup>
-        <FormGroup>
-          <Label for="exampleSelect">Select</Label>
-          <Input type="select" name="select" id="exampleSelect">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </Input>
-        </FormGroup>
+          <MultipleSelection options={options} />
         {
           type === DEPARTAMENTAL &&
-            <FormGroup>
-              <Label for="exampleSelectMulti">Select Multiple</Label>
-              <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-              </Input>
-            </FormGroup>
+            <MultipleSelection options={options} multiple />
         }
         <FormGroup>
           <Label for="exampleText">Text Area</Label>
